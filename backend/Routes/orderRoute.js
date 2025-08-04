0b289292d765c2844a14a70999be6c005d307e95
Saryu Saraswat <21cse107@gweca.ac.in>
2025-08-04 17:40:44 +0530

    milestone 3 completed

--- backend/Routes/orderRoute.js
+++ backend/Routes/orderRoute.js
@@ -8,27 +8,27 @@ const getDB = require('../db');
 router.get('/', async(req, res) => {
     try {
         const db = await getDB();
-        const users = await db.collection('users').find({}).toArray();
+        const orders = await db.collection('orders').find({}).toArray();
 
-        res.status(200).json({ success: true, data : users});
+        res.status(200).json({ success: true, data : orders});
     } catch (error) {
         console.log(error);
         res.status(500).json({
             success : false,
-            message: 'failed to fetch customers'
+            message: 'failed to fetch orders'
         });
     }
 });
 
 router.get('/:id', async(req, res) => {
     try {
-        const user = await User.findOne({id: parseInt(req.params.id)});
+        const order = await User.findOne({id: parseInt(req.params.id)});
 
-        if(!user){
-            return res.status(404).json({success: false, message : 'customer not found'});
+        if(!order){
+            return res.status(404).json({success: false, message : 'order not found'});
         }
 
-        const orderCount = await Order.countDocuments({user_id : user_id});
+        const orderCount = await Order.countDocuments({order_id : order_id});
 
         res.json({ success: true, user, orderCount});
     } catch (error) {
